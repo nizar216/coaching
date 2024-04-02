f6033dde2c7f051021d3c0439c0c2500d779f48b
nizar arous <nizar@datasphera.com>
2024-04-02 10:14:59 +0200

    jwt authentication complete

--- routes/api.php
+++ routes/api.php
@@ -8,13 +8,16 @@ use App\Http\Controllers\Api\JWTAuthController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::middleware('jwt.verify')->group(function() {
-Route::apiResource('players', PlayerController::class);
+Route::middleware('jwt.verify')->group(function () {
+    Route::apiResource('players', PlayerController::class);
 });
 
 Route::post('/register', [JWTAuthController::class, 'register']);
 Route::post('/login', [JWTAuthController::class, 'login']);
 Route::get('/user', [JWTAuthController::class, 'getUser']);
-Route::post('/user',[JWTAuthController::class, 'updateProfile']);
+Route::post('/user', [JWTAuthController::class, 'updateProfile']);
 Route::post('/logout', [JWTAuthController::class, 'logout']);
+Route::get('/hello', function () {
+    return 'app is working';
+});
 
